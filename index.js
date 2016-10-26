b59d7cddc2483cafdada539bf2578a5e62c0fc14
Tim Z <tjkz87@gmail.com>
2016-10-26 11:51:25 -0700

    preload template when server starts

--- index.js
+++ index.js
@@ -7,16 +7,9 @@ const fs = require('fs');
 const app = express();
 const port = config.server.port;
 let dummyInvoice = null;
+let template = null;
 
 app.use(bodyParser.json());
-
-fs.readFile('./dummy_data/invoice_add.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  dummyInvoice = JSON.parse(data);
-  dummyInvoice.invoice.consignor = dummyInvoice.invoice.consignor || {};
-  dummyInvoice.invoice.consignee = dummyInvoice.invoice.consignee || {};
-  dummyInvoice.invoice.deliveryTerm = dummyInvoice.invoice.deliveryTerm || {};
-});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
@@ -25,23 +18,34 @@ app.get('/', (req, res) => {
 app.post('/pdfs', (req, res) => {
   if (!req.body.invoices) {
 
-    return res.status(400).send('Not valid JSON');
+    return res.status(400).send(config.messages.invalid_json);
   }
-
 
   return res.status(201).send('Created');
 });
 
 app.get('/pdfs', (req, res) => {
-  ejs.renderFile(`${__dirname}/template.ejs`, dummyInvoice, (err, str) => {
-    if (err) throw err;
+  res.send(ejs.render(template, dummyInvoice));
+});
 
-    return res.send(str);
+fs.readFile('./dummy_data/invoice.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  dummyInvoice = JSON.parse(data);
+  dummyInvoice.invoice.consignor = dummyInvoice.invoice.consignor || {};
+  dummyInvoice.invoice.consignee = dummyInvoice.invoice.consignee || {};
+  dummyInvoice.invoice.deliveryTerm = dummyInvoice.invoice.deliveryTerm || {};
+  console.log('Dummy invoice loaded');
+});
+
+fs.readFile(`${__dirname}/templates/esf.ejs`, 'utf8', (err, data) => {
+  if (err) throw err;
+  template = data;
+  console.log('Template loaded');
+
+  app.listen(port, () => {
+    console.log('listening on port', port);
   });
 });
 
-app.listen(port, () => {
-  console.log('listening on port', port);
-});
 
 module.exports = app;
