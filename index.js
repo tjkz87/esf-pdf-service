bc6a13da76ea8c89cd912dc52d045f2584f7105d
Tim Z <tjkz87@gmail.com>
2016-10-25 22:44:45 -0700

    add more sections to template

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const config = require('./env/config');
+const fs = require('fs');
 
 const app = express();
 const port = config.server.port;
+let dummyInvoice = null;
 
 app.use(bodyParser.json());
+
+fs.readFile('./dummy_data/invoice_fixed.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  dummyInvoice = JSON.parse(data);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
@@ -23,7 +30,7 @@ app.post('/pdfs', (req, res) => {
 });
 
 app.get('/pdfs', (req, res) => {
-  ejs.renderFile(`${__dirname}/template.ejs`, req.body, (err, str) => {
+  ejs.renderFile(`${__dirname}/template.ejs`, dummyInvoice, (err, str) => {
     if (err) return res.status(500).send('server error');
 
     return res.send(str);
