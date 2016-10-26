f1fa63c9de0d19c215b3c31b2b56659fe13e08d2
Tim Z <tjkz87@gmail.com>
2016-10-26 13:18:21 -0700

    add dummy data to tests and wait until app is fully started before tests

--- test/main.js
+++ test/main.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const request = require('supertest');
 const server = require('../index');
+const fs = require('fs');
+
+let dummmyInvoice = null;
+const agent = request.agent(server);
 
 describe('GET /', () => {
   it('should respond with 200', (done) => {
-    request(server)
+    agent
       .get('/')
       .expect(200, done);
   });
@@ -12,22 +16,36 @@ describe('GET /', () => {
 
 describe('GET /doesnotexist', () => {
   it('should respond with 404', (done) => {
-    request(server)
+    agent
       .get('/doesnotexist')
       .expect(404, done);
   });
 });
 
 describe('POST /pdfs', () => {
+  before((done) => {
+    const dummmyInvoicePath = `${__dirname}/../dummy_data/invoice.json`;
+
+    fs.readFile(dummmyInvoicePath, 'utf8', (err, data) => {
+      if (err) throw err;
+      dummmyInvoice = JSON.parse(data);
+      done();
+    });
+  });
+
+  before((done) => {
+    server.on('appStarted', () => done());
+  });
+
   it('should respond with 201', (done) => {
-    request(server)
+    agent
       .post('/pdfs')
-      .send({ invoices: ['hello!'] })
+      .send({ invoices: [dummmyInvoice] })
       .set('Accept', 'application/json')
       .expect(201, done);
   });
   it('should respond with 400 for bad request', (done) => {
-    request(server)
+    agent
       .post('/pdfs')
       .send('notvalidjson')
       .set('Accept', 'application/json')
